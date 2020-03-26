bd61deb3f4cee651dc0eb16d10a722eeba1cfe1c
samuel <samuel.silv.carvalho@gmail.com>
2020-03-26 00:19:38 -0300

    rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import OngController from  "./controllers/ongController";
 import IncidentController from "./controllers/incidentController";
+import ProfileController from "./controllers/profileController";
+import SessionController from "./controllers/sessionController";
 
 const router = Router();
 
@@ -10,6 +12,10 @@ router.get('/ong', OngController.list);
 router.post('/incident', IncidentController.create);
 router.get('/incident', IncidentController.list);
 router.delete('/incident/:id', IncidentController.delete);
+
+router.get('/profile/incident', ProfileController.list);
+
+router.post('/session/login', SessionController.login);
 
 export default router;
 

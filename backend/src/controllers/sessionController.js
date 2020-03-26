080003037f5ee67cf4b8b5f475de439f2095bd50
samuel <samuel.silv.carvalho@gmail.com>
2020-03-26 00:19:21 -0300

    controller de sessao do usuario

---
+++ backend/src/controllers/sessionController.js
@@ -0,0 +1,20 @@
+import db from "../database/connection";
+
+export class SessionController {
+
+    async login(request, response) {
+        const { id } = request.body;
+
+        const ong = await db('ongs')
+            .where('id', id)
+            .select('name')
+            .first();
+
+        if (!ong)
+            return response.status(400).json({ error: "No ONG found with this ID." });
+
+        return response.json(ong);
+    }
+}
+
+export default new SessionController();

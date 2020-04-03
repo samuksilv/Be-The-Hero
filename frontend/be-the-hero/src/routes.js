881e31cd0a16924117db783db67890e767aa5301
samuel.carvalho <samuel.carvalho@mag-it.com.br>
2020-04-03 08:21:39 -0300

    # frontend

--- frontend/be-the-hero/src/routes.js
+++ frontend/be-the-hero/src/routes.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ToastContainer } from "react-toastify";
+
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
@@ -7,13 +9,26 @@ import NewIncident from './pages/NewIncident';
 
 export default function Routes() {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" component={Logon} exact />
-                <Route path="/register" component={Register} exact />
-                <Route path="/profile" component={Profile} exact />
-                <Route path="/incidents/new" component={NewIncident} exact />
-            </Switch>
-        </BrowserRouter>
+        <>
+            <ToastContainer
+                position="top-right"
+                autoClose={6500}
+                hideProgressBar={false}
+                newestOnTop
+                closeOnClick={false}
+                rtl={false}
+                pauseOnVisibilityChange
+                draggable
+                pauseOnHover
+            />
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" component={Logon} exact />
+                    <Route path="/register" component={Register} exact />
+                    <Route path="/profile" component={Profile} exact />
+                    <Route path="/incidents/new" component={NewIncident} exact />
+                </Switch>
+            </BrowserRouter>
+        </>
     )
 }
